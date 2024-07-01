c0b566eeb0132d94ad63bfc9a9af43e942576813
Felipe <felipesilfaria@gmail.com>
2024-07-01 19:24:33 -0300

    Finalizando parte do Felipe

--- codigo/Login/login.js
+++ codigo/Login/login.js
@@ -5,19 +5,20 @@ document
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    fetch("users.json")
+    fetch("../data.json")
       .then((response) => response.json())
-      .then((users) => {
+      .then((data) => {
+        const users = data.users;
         const user = users.find(
           (user) => user.login === username && user.senha === password
         );
         if (user) {
           alert("Login realizado com sucesso!");
-          // Aqui você pode redirecionar o usuário para outra página
+          window.location.href = "../Home/home.html"; // Redireciona para a página home.html
         } else {
           document.getElementById("error-message").textContent =
             "Usuário ou senha incorretos.";
         }
       })
-      .catch((error) => console.error("Erro ao carregar usuários:", error));
+      .catch((error) => console.error("Erro ao carregar dados:", error));
   });
