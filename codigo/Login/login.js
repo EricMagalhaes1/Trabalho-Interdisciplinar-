299138cff82ab63c294f259b48a7d28872db82ed
Jean Carlos <jeanlopes2166@gmail.com>
2024-07-01 20:46:31 -0300

    Login Fix

--- codigo/Login/login.js
+++ codigo/Login/login.js
@@ -1,24 +1,27 @@
-document
-  .getElementById("loginForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+document.getElementById("loginForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-    fetch("../data.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const users = data.users;
-        const user = users.find(
-          (user) => user.login === username && user.senha === password
-        );
-        if (user) {
-          alert("Login realizado com sucesso!");
-          window.location.href = "../Home/home.html"; // Redireciona para a página home.html
-        } else {
-          document.getElementById("error-message").textContent =
-            "Usuário ou senha incorretos.";
-        }
-      })
-      .catch((error) => console.error("Erro ao carregar dados:", error));
-  });
+  fetch("../data.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const users = data.users;
+      const user = users.find(
+        (user) => user.login === username && user.senha === password
+      );
+      if (user) {
+        alert("Login realizado com sucesso!");
+        window.location.href = "../Home/home.html"; // Redireciona para a página home.html
+      } else {
+        document.getElementById("error-message").textContent =
+          "Usuário ou senha incorretos.";
+      }
+    })
+    .catch((error) => console.error("Erro ao carregar dados:", error));
+});
